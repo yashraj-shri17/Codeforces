54a2b5ae3efc5fb752c9a45c6d50f6b5679b1562
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-02-12 23:30:57 +0530

    Beautiful Matrix,

---
+++ Beautiful Matrix,.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() {
+    int arr[5][5];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int row, col;
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (arr[i][j] == 1) {
+                row = i;
+                col = j;
+                break;
+            }
+        }
+    }
+    int moves = abs(row-2) + abs(col-2);
+    printf("%d", moves);
+    return 0;
+}
