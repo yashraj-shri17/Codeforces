7d75d42c29e8cc74cfdbbfcbccbca6d8cd36292a
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-03-05 22:46:33 +0530

    Chat room

---
+++ Chat room.py
@@ -0,0 +1,11 @@
+def can_say_hello(s):
+    hello = "hello"
+    i = 0
+    for char in s:
+        if char == hello[i]:
+            i += 1
+            if i == len(hello):
+                return "YES"
+    return "NO"
+s = input().strip()
+print(can_say_hello(s))
