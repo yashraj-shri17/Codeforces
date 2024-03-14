650299f89cd0de9dbe11f64604d9162edf447f73
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-03-14 22:44:52 +0530

    BubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,15 @@
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+arr = [64, 34, 25, 12, 22, 11, 90]
+print("Original array:", arr)
+
+bubbleSort(arr)
+
+print("Sorted array:", arr)
+
+# The Time Complexity of this code is O(n^2)
