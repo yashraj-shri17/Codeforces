68252119427a1e59e714107b6d4c521db688bd3a
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-03-17 22:43:13 +0530

    LongestIncSubseq

---
+++ LongestIncSubseq.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> dp(n, 1);
+    
+    for (int i = 1; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            if (nums[i] > nums[j]) {
+                dp[i] = max(dp[i], dp[j] + 1);
+            }
+        }
+    }
+    
+    int maxLength = 0;
+    for (int len : dp) {
+        maxLength = max(maxLength, len);
+    }
+    
+    return maxLength;
+}
+
+int main() {
+    vector<int> nums = {10, 9, 2, 5, 3, 7, 101, 18};
+    cout << "Length of Longest Increasing Subsequence: " << lengthOfLIS(nums) << endl;
+    return 0;
+}
