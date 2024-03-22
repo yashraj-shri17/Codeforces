ee85a6ac6e627aa1f8b1ad29b333584a5d8e7d96
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-03-22 17:51:57 +0530

    Too Min Too Max

--- Too Min Too Max.py
+++ Too Min Too Max.py
@@ -4,6 +4,7 @@ for i in range(t):
     # SCANNING TEST_VARIABLE
     n = int(input())
     a = list(map(int, input().split()))
+    # SORTING THE LIST
     a = sorted(a)
     ans = 2 * (a[n - 1] - a[0] + a[n - 2] - a[1])
     print(ans)
