d438bd4db0763de644271a4acc2a3d7dab99d0bb
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-03-22 17:49:09 +0530

    Too Min Too Max

---
+++ Too Min Too Max.py
@@ -0,0 +1,7 @@
+t = int(input())
+for i in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    a = sorted(a)
+    ans = 2 * (a[n - 1] - a[0] + a[n - 2] - a[1])
+    print(ans)
