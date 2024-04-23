196dc2a06002b65e018d8b3c9778f33dd713427f
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-23 22:19:18 +0530

    Rating ~ 1200+

--- Cutting Carrot.py
+++ Cutting Carrot.py
@@ -1,4 +1,4 @@
 n, h = map(int, input().split())
 for i in range(1,n):
-    x = h* (i/n)**(1/2) #Formual to find the
+    x = h* (i/n)**(1/2) #Formual to find the height for 
     print(x,end = " " )
