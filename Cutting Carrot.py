9d8bf25f22f52e4a0ee444beee13b6732e740011
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-23 22:19:44 +0530

    Rating ~ 1200+

--- Cutting Carrot.py
+++ Cutting Carrot.py
@@ -1,4 +1,4 @@
 n, h = map(int, input().split())
 for i in range(1,n):
-    x = h* (i/n)**(1/2) #Formual to find the height for 
+    x = h* (i/n)**(1/2) #Formual to find the height for which the areas of given are all equal
     print(x,end = " " )
