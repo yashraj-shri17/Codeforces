0293d9534582e5f05380364cd51e975723a3f97d
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-24 19:59:34 +0530

    Rating~1000

--- Lucky Division.c
+++ Lucky Division.c
@@ -6,6 +6,8 @@ int main() {
 //  initialising variable
         scanf("%d",&n); 
 // scanning var
+//checking if a number is divisible by a lucky number or not in range [1,n]
+
         if(n%4 == 0 || n%7==0 || n%44 == 0 || n%77==0 || n%47==0 || n%74==0 || n%474==0 || n%447==0 || n%744==0 || n%774==0 || n%747==0 || n%747==0 || n%477==0 ){
             printf("YES");
         }
