3348faa4c4911e7f7f3206dfce690c8af48f9d95
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-24 19:57:21 +0530

    Rating~1000

--- Lucky Division.c
+++ Lucky Division.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
  
 int main() {
-    int n;
+    int n; 
+//  initialising variable
     scanf("%d",&n);
         if(n%4 == 0 || n%7==0 || n%44 == 0 || n%77==0 || n%47==0 || n%74==0 || n%474==0 || n%447==0 || n%744==0 || n%774==0 || n%747==0 || n%747==0 || n%477==0 ){
             printf("YES");
