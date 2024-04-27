d8c2216b6d0d71dc768ca815992a291c43c460db
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-27 22:45:21 +0530

    TLE ELIMINATORS (800+)

---
+++ Pretend and Append.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        int n;
+        cin >> n;
+        vector<char> v(n);
+        for(int j = 0; j < n; j++) {
+           cin >> v[j] ; 
+        }
+        int x = n-1;
+        int count = 0;
+        for(int p = 0 ; p < n; p++){
+            if(v[p] != v[x]) { // Compare directly with '\n'
+                count += 2;
+                x--;
+            }
+            else{
+                break;
+            }
+        }
+        if(n-count > 0){
+        cout << n - count << endl;
+        }
+        else{
+            cout << 0 << endl;
+        }
+    }
+    return 0;
+}
