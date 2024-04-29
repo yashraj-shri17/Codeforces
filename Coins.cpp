92493579f1baaab0b7f52f95725992eedb510e4a
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-29 21:56:34 +0530

    TLE ELIMINATORS (800+)

---
+++ Coins.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        long long n, k;
+        cin >> n >> k;
+
+        if (k % 2 == 0 && n % 2 != 0) {
+            cout << "NO" << endl;
+        } else if (k % 2 == 0 && n % 2 == 0) {
+            cout << "YES" << endl;
+        } else {
+            for (long long y = 0; y >= 0; y++) {
+                if (y % 2 != 0 && k % 2 != 0 && n % 2 != 0) {
+                    cout << "YES" << endl;
+                    break;
+                } else if (y % 2 == 0 && k % 2 != 0 && n % 2 == 0) {
+                    cout << "YES" << endl;
+                    break;
+                }
+            }
+        }
+    }
+
+    return 0;
+}
