d755d7ce6cc3193d98be50f1e708e9078b53799b
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-04-30 09:45:15 +0530

    TLE ELIMINATORS (800+)

---
+++ Everybody Like Good Arrays.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m;
+    cin>>m;
+    while(m--){
+        int n;
+        cin>>n;
+        vector<int> v;
+        for(int i=0;i<n;i++){
+            int x;
+            cin>>x;
+            v.push_back(x);
+        }
+        int count=0;
+        for(int i=0;i<v.size()-1;i++){
+            if((v[i]%2!=0 && v[i+1]%2!=0)||(v[i]%2==0 && v[i+1]%2==0) ){
+                v[i+1]*=v[i];
+                v.erase(v.begin() + i);
+                i--;
+                count++;
+            }
+        }
+        cout<<count<<endl;
+        
+    }
+}
