0ff27b9409e43523c23708d5c77f25ca3f652436
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-05-03 18:03:24 +0530

    TLE ELIMINATORS (800+)

---
+++ Ambitious Kid.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    int minAbs = abs(v[0]);
+    for(int j = 1; j < n; j++) {
+        minAbs = min(minAbs, abs(v[j]));
+    }
+
+    cout << minAbs << endl;
+    return 0;
+}
