7ef7a2caec67b41ea715049c8a2a5303ff85445d
yashraj-shri17 <158489592+yashraj-shri17@users.noreply.github.com>
2024-05-06 19:44:27 +0530

    TLE ELIMINATORS (900+)

---
+++ 01 Game.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    for(int i = 0; i < t; i++){
+        string s;
+        cin >> s;
+        int count = 0;
+        for(int j = 0; j < s.size();) {
+            if((s[j]=='0' && s[j+1]=='1') || (s[j]=='1' && s[j+1]=='0') ){
+                count++;
+                s.erase(j, 2);
+                j = max(0, j - 1);
+            } else {
+                j++;
+            }
+        }
+        if(count % 2 == 0){
+            cout << "NET" << endl;
+        }
+        else{
+            cout << "DA" << endl;
+        }
+    }
+    return 0;
+}
